f906c6a6e8b60e350aaf136af06f0e5d56acc9bc
rorikata <orirei0829@gmail.com>
2020-12-01 11:50:52 -0800

    test

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -23,6 +23,7 @@ middleware.errorHandler = (app) => {
 
   // error handler
   app.use((err, req, res) => {
+    console.log(err);
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
